5d505f47d627b32cb031c6f5f915a1e05f7fb8b7
FelipeSchimidt <felipeschimidt85@gmail.com>
2019-09-20 09:54:46 -0300

    CRUD do controller usuario

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
-using System.Net;
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using aspApi.Models;
 using aspApi.Database;
 
@@ -13,7 +11,7 @@ namespace aspApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsuarioController
+    public class UsuarioController : ControllerBase
     {
         private ApiDBContext context;
 
@@ -23,24 +21,73 @@ namespace aspApi.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Usuario> Get()
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
         {
-            return context.Usuarios.ToList();
+            return await context.Usuarios.ToListAsync();
         }
 
-        [HttpGet("user")]
-        public HttpResponseMessage GetUsuario(int id)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
         {
-            var usuario = context.Usuarios.FirstOrDefault(u => u.id == id);
+            var usuario = await context.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
         }
 
         [HttpPost]
-        public Usuario Post(Usuario usuario)
+        public async Task<ActionResult<Usuario>> CreateUsuario(Usuario usuario)
         {
             context.Usuarios.Add(usuario);
-            context.SaveChanges();
+            await context.SaveChangesAsync();
+
+            /** 
+            * CreatedAtAction
+            * 1º - Retorna STATUSCODE = 201 e adiciona ao Location a resposta
+            * 2º - Refencia a ação GetUsuario
+            * 3º - retorna usuário no qual usuario.include == id
+            */
+            return CreatedAtAction(nameof(GetUsuario),
+                new { id = usuario.id },
+                usuario
+                );
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Usuario>> UpdateUsuario(int id, Usuario usuario)
+        {
+            if (id != usuario.id)
+            {
+                return BadRequest();
+            }
+
+            /**
+            * Especifica uma entitdade que tera seu estado alterado
+            */
+            context.Entry(usuario).State = EntityState.Modified;
+            await context.SaveChangesAsync();
 
             return usuario;
+
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Usuario>> DeleteUsuario(int id)
+        {
+            var user = await context.Usuarios.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            context.Usuarios.Remove(user);
+            await context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
